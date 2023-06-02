412208021fd9634b0b788899b9791db19d867749
Raj Choutkuri <unix72@gmail.com>
2023-06-02 20:03:56 +0000

    nisburgh notes commit 0602

--- lab03/pageview-container/main.go
+++ lab03/pageview-container/main.go
@@ -10,12 +10,12 @@ var pageView int64
 
 func main() {
         http.HandleFunc("/", handler)
-        log.Printf("Server v1.0 starting and listening on 0.0.0.0:8080...")
+        log.Printf("Server v2.0 starting and listening on 0.0.0.0:8080...")
         log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
         log.Printf("Ping from %s", r.RemoteAddr)
         pageView++
-        fmt.Fprintf(w, "[v1.0] Hello, you're visitor #%d !", pageView)
+        fmt.Fprintf(w, "[v2.0] Hello, you're visitor #%d !", pageView)
 }
